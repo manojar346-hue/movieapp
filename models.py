91e0613d3d0af92428ae96e7ed08044995f445c0
manojar346-hue <manojar346@gmail.com>
2026-04-10 19:08:26 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    poster = db.Column(db.String(200), nullable=False)
